19c82d76fa1c18e59777ba9cbe50f0dc63404aa9
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2026-01-18 22:21:20 +0000

    implement GET endpoint to fetch all questions from database

---
+++ src/main/java/com/quizapp/project/service/QuestionService.java
@@ -0,0 +1,19 @@
+package com.quizapp.project.service;
+
+import com.quizapp.project.Question;
+import com.quizapp.project.dao.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public List<Question> getAllQuestions() {
+        return questionDao.findAll();
+    };
+}
